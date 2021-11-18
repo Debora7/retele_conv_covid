cfcb44e3d46c1ebd7fec703ea989c42ffccf2642
Debora7 <burnichinaomi@gmail.com>
2021-11-18 18:31:06 +0200

    Crearea fisierului functions.py

--- retele_conv_covid_2.py
+++ retele_conv_covid_2.py
@@ -3,8 +3,10 @@
 
 # In[8]:
 
-
 import tensorflow as tf
+import yaml
+import matplotlib.pyplot as plt
+
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Conv2D
@@ -15,46 +17,15 @@ from tensorflow.keras.layers import BatchNormalization
 from numpy import unique
 from numpy import argmax
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import yaml
-import matplotlib.pyplot as plt
 from tensorflow.keras.applications import VGG16
+from functions import *
 
 config = None
 with open('config.yml') as f:  
     config = yaml.load(f)
 
-
-# Functia de plotare a acuratetei si a functiei loss
-def plot_acc_loss(result):
-    acc = result.history['accuracy']
-    loss = result.history['loss']
-    val_acc = result.history['val_accuracy']
-    val_loss = result.history['val_loss']
-    plt.figure(figsize=(15, 5))
-    plt.subplot(121)
-    plt.plot(acc, label='Train')
-    plt.plot(val_acc, label='Validation')
-    plt.title('Accuracy', size=15)
-    plt.legend()
-    plt.grid(True)
-    plt.ylabel('Accuracy')
-    plt.xlabel('Epoch')
-
-    plt.subplot(122)
-    plt.plot(loss, label='Train')
-    plt.plot(val_loss, label='Validation')
-    plt.title('Loss', size=15)
-    plt.legend()
-    plt.grid(True)
-    plt.ylabel('Loss')
-    plt.xlabel('Epoch')
-
-    plt.show()
-    
-
 # data_path = config['paths']['path_small']  # Small_dataset
 data_path = config['paths']['path_big']  # Big_dataset
-
 
 # train_datagen = ImageDataGenerator(rescale=1./255)
 train_datagen = ImageDataGenerator(**config['img_gen'])
